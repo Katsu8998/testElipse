4f907b70d404315bc302d68469cd305a4fb9f406
Katsu8998 <dailywork8998@gmail.com>
2022-11-29 20:36:19 +0900

    Additional Message

--- testGit2/src/Test/gitTest.java
+++ testGit2/src/Test/gitTest.java
@@ -4,7 +4,8 @@ public class gitTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello GitHub!");
-
+		System.out.println("Connecting with GitHub is successfull!");
 	}
 
 }
+
